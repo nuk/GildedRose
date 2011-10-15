3383b82068e833d878c07487fe5c2f33e13da725
raptor8 <raptor8@raptor8.(none)>
2011-10-15 15:34:51 -0300

    Digdin FC(with Matheus)

--- src/gildedrose/console/HotelConsole.java
+++ src/gildedrose/console/HotelConsole.java
@@ -16,40 +16,41 @@ public class HotelConsole {
 			if (Items.get(i).Name == "Sulfuras, Hand of Ragnaros") {
 
 			} else {
+				if (Items.get(i).Name == "Aged Brie") {
+					updateBrie(Items.get(i));
+				}else if (Items.get(i).Name == "Backstage passes to a TAFKAL80ETC concert"){
+					updatePasse(Items.get(i));
+				}else { //todo resto
+					updateItem(Items.get(i));
+					Items.get(i).SellIn = Items.get(i).SellIn - 1;
+					
+					if (Items.get(i).Name != "Aged Brie"
+							&& Items.get(i).Name != "Backstage passes to a TAFKAL80ETC concert") {
+						if (Items.get(i).Quality > 0) {
+								Items.get(i).Quality--;
+						}
+					} else {// se for queijo ou passe
+						if (Items.get(i).Quality < 50) {
+							Items.get(i).Quality ++;
 
-				if (Items.get(i).Name != "Aged Brie"
-						&& Items.get(i).Name != "Backstage passes to a TAFKAL80ETC concert") {// Se
-					// não
-					// for
-					// queijo
-					// e
-					// se
-					// não
-					// for
-					// passe
-					if (Items.get(i).Quality > 0) {
-							Items.get(i).Quality--;
-					}
-				} else {// se for queijo ou passe
-					if (Items.get(i).Quality < 50) {
-						Items.get(i).Quality = Items.get(i).Quality + 1;
-
-						if (Items.get(i).Name == "Backstage passes to a TAFKAL80ETC concert") {
-							if (Items.get(i).SellIn < 11) {
-								if (Items.get(i).Quality < 50) {
-									Items.get(i).Quality = Items.get(i).Quality + 1;
+							if (Items.get(i).Name == "Backstage passes to a TAFKAL80ETC concert") {
+								if (Items.get(i).SellIn < 11) {
+									if (Items.get(i).Quality < 50) {
+										Items.get(i).Quality++;
+									}
 								}
-							}
 
-							if (Items.get(i).SellIn < 6) {
-								if (Items.get(i).Quality < 50) {
-									Items.get(i).Quality = Items.get(i).Quality + 1;
+								if (Items.get(i).SellIn < 6) {
+									if (Items.get(i).Quality < 50) {
+										Items.get(i).Quality = Items.get(i).Quality + 1;
+									}
 								}
 							}
 						}
 					}
+					
 				}
-
+				
 				Items.get(i).SellIn = Items.get(i).SellIn - 1;
 
 				if (Items.get(i).SellIn < 0) {// validade acabou
@@ -72,4 +73,47 @@ public class HotelConsole {
 			}
 		}
 	}// for
+	
+	private void updateItem(Item item) {
+		item.SellIn--;
+		
+	}
+
+	private void updateBrie(Item item){
+		if (item.Quality < 50) {
+			item.Quality ++;
+
+			if (item.Name == "Backstage passes to a TAFKAL80ETC concert") {
+				if (item.SellIn < 11) {
+					if (item.Quality < 50) {
+						item.Quality++;
+					}
+				}
+
+				if (item.SellIn < 6) {
+					if (item.Quality < 50) {
+						item.Quality = item.Quality + 1;
+					}
+				}
+			}
+		}
+	}
+	
+	private void updatePasse(Item item){
+		if (item.Quality < 50) {
+			item.Quality ++;
+
+			if (item.SellIn < 11) {
+				if (item.Quality < 50) {
+					item.Quality++;
+				}
+			}
+
+			if (item.SellIn < 6) {
+				if (item.Quality < 50) {
+					item.Quality = item.Quality + 1;
+				}
+			}
+		}
+	}
 }
