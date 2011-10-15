3f3ab0243c95bfb0b593e413706f29df4e491651
Matheus Assis <matheus_hk@yahoo.com>
2011-10-15 16:57:32 -0300

    For Dumbledore.

--- src/gildedrose/console/HotelConsole.java
+++ src/gildedrose/console/HotelConsole.java
@@ -10,8 +10,7 @@ public class HotelConsole {
 		this.Items = Items;
 	}
 	
-	public void fixNegativeQuality(List<Item> items){
-		for(Item item: items){
+	public void fixQuality(Item item){
 			if(item.Quality < 0){
 				item.Quality = 0;
 			}
@@ -22,84 +21,79 @@ public class HotelConsole {
 			}else{
 				item.Quality = 80;
 			}
+	}
+	
+	public void updateItemSellIn(Item item) {
+		item.SellIn -=1;
+	}
+	
+	public void updateItemQuality(Item item){
+		if (item.Quality > 0){
+			item.Quality -= (item.SellIn>0) ? 1:2;
 		}
+	}
+	
+	public void updateAgedItemQuality(Item item){
+		if (item.Quality < 50){
+			item.Quality += 1;
+		}
+		if (item.Quality < 50 && item.SellIn <=0){
+			item.Quality += 1;
+		}
+	}
+	
+	public void updateEventTicketQuality(Item item){
+		if (item.SellIn > 10){
+			item.Quality +=1;
+		}else if(item.SellIn > 5){
+			item.Quality += 2;
+		}else if(item.SellIn > 0){
+			item.Quality +=3;
+		}else{
+			item.Quality =0;
+		}
+	} 
+	
+	public boolean isAgedItem(Item item){
+		if (item.Name.equals("Aged Brie")){
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean isEventTicket(Item item){
+		if (item.Name.equals("Backstage passes to a TAFKAL80ETC concert")){
+			return true;
+		}
+		return false;
+	}	
+	
+	public boolean isLegendaryItem(Item item){
+		if (item.Name.equals("Sulfuras, Hand of Ragnaros")){
+			return true;
+		}
+		return false;
 	}
 
     public void UpdateQuality()
     {
         for (int i = 0; i < Items.size(); i++)
         {
-            if (!Items.get(i).Name.equals("Aged Brie") && !Items.get(i).Name.equals("Backstage passes to a TAFKAL80ETC concert"))
-            {
-                if (Items.get(i).Quality > 0)
-                {
-                    if (!Items.get(i).Name.equals("Sulfuras, Hand of Ragnaros"))
-                    {
-                        Items.get(i).Quality = Items.get(i).Quality - 1;
-                    }
-                }
-            }
-            else
-            {
-                if (Items.get(i).Quality < 50)
-                {
-                    Items.get(i).Quality = Items.get(i).Quality + 1;
-
-                    if (Items.get(i).Name.equals("Backstage passes to a TAFKAL80ETC concert"))
-                    {
-                        if (Items.get(i).SellIn < 11)
-                        {
-                            if (Items.get(i).Quality < 50)
-                            {
-                                Items.get(i).Quality = Items.get(i).Quality + 1;
-                            }
-                        }
-
-                        if (Items.get(i).SellIn < 6)
-                        {
-                            if (Items.get(i).Quality < 50)
-                            {
-                                Items.get(i).Quality = Items.get(i).Quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!Items.get(i).Name.equals("Sulfuras, Hand of Ragnaros"))
-            {
-                Items.get(i).SellIn = Items.get(i).SellIn - 1;
-            }
-
-            if (Items.get(i).SellIn < 0)
-            {
-                if (!Items.get(i).Name.equals("Aged Brie"))
-                {
-                    if (!Items.get(i).Name.equals("Backstage passes to a TAFKAL80ETC concert"))
-                    {
-                        if (Items.get(i).Quality > 0)
-                        {
-                            if (!Items.get(i).Name.equals("Sulfuras, Hand of Ragnaros"))
-                            {
-                                Items.get(i).Quality = Items.get(i).Quality - 1;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        Items.get(i).Quality = Items.get(i).Quality - Items.get(i).Quality;
-                    }
-                }
-                else
-                {
-                    if (Items.get(i).Quality < 50)
-                    {
-                        Items.get(i).Quality = Items.get(i).Quality + 1;
-                    }
-                }
-            }
+        	Item item = Items.get(i);
+        	if (!isLegendaryItem(item)){
+        		if (isAgedItem(item)){
+        			updateAgedItemQuality(item);
+        		}else if (isEventTicket(item)){
+        			updateEventTicketQuality(item);
+        		}else{
+        			updateItemQuality(item);
+        		}
+        		updateItemSellIn(item);
+        	}
+        	fixQuality(item); 
         }
-        fixNegativeQuality(Items);
     }
+
+	
 }
 
