606b56d085c42ba90f1d3cf72f50c6ba551de193
raptor8 <raptor8@raptor8.(none)>
2011-10-15 14:56:08 -0300

    Digdin FC enviando primeira modificacao

--- src/gildedrose/console/HotelConsole.java
+++ src/gildedrose/console/HotelConsole.java
@@ -15,17 +15,17 @@ public class HotelConsole {
         for (int i = 0; i < Items.size(); i++)
         {
             if (Items.get(i).Name != "Aged Brie" && Items.get(i).Name != "Backstage passes to a TAFKAL80ETC concert")
-            {
+            {// Se não for queijo e se não for passe
                 if (Items.get(i).Quality > 0)
                 {
                     if (Items.get(i).Name != "Sulfuras, Hand of Ragnaros")
-                    {
+                    {//vale pro resto
                         Items.get(i).Quality = Items.get(i).Quality - 1;
                     }
                 }
             }
             else
-            {
+            {//se for queijo ou passe
                 if (Items.get(i).Quality < 50)
                 {
                     Items.get(i).Quality = Items.get(i).Quality + 1;
@@ -52,12 +52,12 @@ public class HotelConsole {
             }
 
             if (Items.get(i).Name != "Sulfuras, Hand of Ragnaros")
-            {
+            {//vale pro resto menos Sulfuras
                 Items.get(i).SellIn = Items.get(i).SellIn - 1;
             }
 
             if (Items.get(i).SellIn < 0)
-            {
+            {//validade acabou
                 if (Items.get(i).Name != "Aged Brie")
                 {
                     if (Items.get(i).Name != "Backstage passes to a TAFKAL80ETC concert")
@@ -66,20 +66,20 @@ public class HotelConsole {
                         {
                             if (Items.get(i).Name != "Sulfuras, Hand of Ragnaros")
                             {
-                                Items.get(i).Quality = Items.get(i).Quality - 1;
+                                Items.get(i).Quality--;
                             }
                         }
-                    }
+                    }//2° if
                     else
-                    {
-                        Items.get(i).Quality = Items.get(i).Quality - Items.get(i).Quality;
+                    {//nao acontece com queijo
+                        Items.get(i).Quality = 0;
                     }
-                }
+                }//1° if
                 else
                 {
                     if (Items.get(i).Quality < 50)
                     {
-                        Items.get(i).Quality = Items.get(i).Quality + 1;
+                        Items.get(i).Quality++;
                     }
                 }
             }
